2b9caa0c79f73297a271077f36c76c254199845a
i.manzhela <illiamanzhela01@gmail.com>
2025-02-08 15:18:59 +0100

    reading js file from cli argument moved to a separate function

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
 mod scanner;
 
-use std::{env, fs, io, path::Path};
+use std::{env::{self, Args}, fs, io, path::Path};
 use scanner::Scanner;
 
-fn main() -> io::Result<()> {
-    let path = env::args().nth(1).expect("<path> is not provided");
+fn get_js_content(mut args: Args) -> io::Result<String> {
+    let path: String = args.nth(1).expect("<path> is not provided");
     let path = Path::new(&path);
     if path.extension().and_then(|ext| ext.to_str()) != Some("js") {
         return Err(io::Error::new(io::ErrorKind::Other,"only .js files are accepted"));
     }
 
-    let source = fs::read_to_string(path)?;
-    let mut scanner = Scanner::new(source);
+    Ok(fs::read_to_string(path)?)
+}
 
+fn main() -> io::Result<()> {
+    let source = get_js_content(env::args())?;
+
+    let mut scanner = Scanner::new(source);
     let tokens = scanner.tokenize();
+    
     for token in tokens {
         println!("{:?}", token);
     }
