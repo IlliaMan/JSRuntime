87d42e36826cc3e0747cde50a9c3d91209647820
i.manzhela <illiamanzhela01@gmail.com>
2025-02-10 08:40:50 +0100

    primitives scanning added

--- src/scanner/scanner.rs
+++ src/scanner/scanner.rs
@@ -57,6 +57,11 @@ impl Scanner {
                         tokens.push(self.consume_identifier());
                         continue;
                     }
+
+                    if c == '"' || c == '\'' || c == '`'  {
+                        tokens.push(self.consume_string());
+                        continue;
+                    }
                 },
                 kind => tokens.push(Token::new(kind, self.position /* TODO: should be line */)),
             }
@@ -100,11 +105,31 @@ impl Scanner {
 
         while !self.is_end() && (self.peek().is_ascii_alphanumeric() || self.peek() == '_') {
             identifier.push(self.peek());
-            self.position += 1;
+            self.increment_position();
         }
 
         let token_type = TokenType::from(&identifier.chars().collect::<Vec<char>>()[..]);
 
+        Token::new(token_type, self.position /* TODO: should be line */)
+    }
+
+    fn consume_string(&mut self) -> Token {
+        let mut string = String::new();
+        let quote = self.peek();
+        self.increment_position();
+
+        while !self.is_end() && self.peek() != quote {
+            string.push(self.peek());
+            self.increment_position();
+        }
+
+        if self.is_end() {
+            panic!("string {} has no closing quote {}", string, quote);
+        }
+
+        let token_type = TokenType::String(String::from(string));
+        self.increment_position();
+        
         Token::new(token_type, self.position /* TODO: should be line */)
     }
 
